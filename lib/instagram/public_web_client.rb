c4b4bcf8571bdd0d9ccd5fa3377ece4b41cd1bde
Peter Toth <tothpeter08@gmail.com>
2021-07-17 17:08:28 -0500

    Add ability to fetch any number of the latest posts of an instagram user

--- lib/instagram/public_web_client.rb
+++ lib/instagram/public_web_client.rb
@@ -11,14 +11,35 @@ module Instagram::PublicWebClient
     JSON.parse(response, symbolize_names: true)
   end
 
-  def self.fetch_latest_posts_of(user_name)
-    url = "https://www.instagram.com/#{user_name}/?__a=1"
-    uri = URI(url)
-    response = Net::HTTP.get(uri)
+  def self.fetch_latest_posts_by_user_name(user_name, limit: 50)
+    profile_data = fetch_profile(user_name)
+    user_id      = profile_data[:graphql][:user][:id]
 
-    json_data = JSON.parse(response, symbolize_names: true)
+    fetch_lates_posts_by_user_id(user_id, limit: limit)
+  end
 
-    json_data[:graphql][:user][:edge_owner_to_timeline_media][:edges]
-      .map { |edge| edge[:node] }
+  def self.fetch_lates_posts_by_user_id(user_id, limit: 50)
+    posts = []
+    number_of_remaining_posts_to_fetch = limit
+
+    url = "https://www.instagram.com/graphql/query/?query_hash=ea4baf885b60cbf664b34ee760397549&variables={\"id\":\"#{user_id}\",\"first\":#{number_of_remaining_posts_to_fetch}}"
+
+    loop do
+      uri = URI(url)
+      response = Net::HTTP.get(uri)
+      json_data = JSON.parse(response, symbolize_names: true)
+
+      fetched_posts = json_data[:data][:user][:edge_owner_to_timeline_media][:edges].map { |edge| edge[:node] }
+      posts += fetched_posts
+
+      number_of_remaining_posts_to_fetch -= fetched_posts.count
+      page_info = json_data[:data][:user][:edge_owner_to_timeline_media][:page_info]
+
+      break if number_of_remaining_posts_to_fetch < 1 || !page_info[:has_next_page]
+
+      url = "https://www.instagram.com/graphql/query/?query_hash=ea4baf885b60cbf664b34ee760397549&variables={\"id\":\"#{user_id}\",\"first\":#{number_of_remaining_posts_to_fetch},\"after\":\"#{page_info[:end_cursor]}\"}"
+    end
+
+    posts
   end
 end
--- spec/lib/instagram/public_web_client_spec.rb
+++ spec/lib/instagram/public_web_client_spec.rb
@@ -11,14 +11,15 @@ describe Instagram::PublicWebClient do
     end
   end
 
-  vcr_options = { cassette_name: 'instagram/profile/posts_last_12', match_requests_on: [:path] }
+  vcr_options = { cassette_name: 'instagram/profile/posts_last_130', match_requests_on: [:path] }
 
-  describe 'Fetching the last 12 posts of a given profile', vcr: vcr_options do
+  describe 'Fetching limited posts of a given profile', vcr: vcr_options do
     it 'returns the posts' do
-      posts = described_class.fetch_latest_posts_of('captain_rebel_tv')
+      posts = described_class.fetch_latest_posts_by_user_name('captain_rebel_tv', limit: 130)
 
-      expect(posts.count).to eq(12)
+      expect(posts.count).to eq(130)
       expect(posts.first[:id]).to eq('2618499570550571667')
+      expect(posts.last[:id]).to eq('2404465245923214590')
     end
   end
 end
